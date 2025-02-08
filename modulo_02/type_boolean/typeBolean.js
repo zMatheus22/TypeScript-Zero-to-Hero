361f3809a1441278ddfc0dd86aae69a6b81f69d7
Matheus Vinicyus Strada <57578830+zMatheus22@users.noreply.github.com>
2025-02-08 21:42:20 +0000

    Type `Boolean` in TypeScript

---
+++ modulo_02/type_boolean/typeBolean.js
@@ -0,0 +1,14 @@
+"use strict";
+// => Exemplo 01
+let tarefaConcluida = true;
+let tarefaPendente = false;
+console.log(tarefaConcluida);
+console.log(tarefaPendente);
+// => Exemplo 02
+let concluida = false;
+if (concluida) {
+    console.log("Tarefa concluida!");
+}
+else {
+    console.log("Tarefa pendente!");
+}
---
+++ modulo_02/type_boolean/typeBolean.ts
@@ -0,0 +1,16 @@
+// => Exemplo 01
+let tarefaConcluida: boolean = true;
+let tarefaPendente: boolean = false;
+
+console.log(tarefaConcluida);
+console.log(tarefaPendente);
+
+// => Exemplo 02
+
+let concluida: boolean = false;
+
+if (concluida) {
+  console.log("Tarefa concluida!");
+} else {
+  console.log("Tarefa pendente!");
+}
