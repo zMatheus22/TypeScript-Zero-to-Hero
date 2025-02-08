9eb8b5fd36f2524646f025e397ffeffa76e9fce8
Matheus Vinicyus Strada <57578830+zMatheus22@users.noreply.github.com>
2025-02-08 22:15:57 +0000

    Type `string` in TypeScript

---
+++ modulo_02/type_string/typeString.js
@@ -0,0 +1,11 @@
+"use strict";
+// => Exemplo 01 - Single Quote
+let nomeCompleto = "Matheus Vinicyus Strada";
+console.log(nomeCompleto);
+// => Exemplo 02 - Double Quote
+let funcaoEmpresa = "Analista de Suporte";
+console.log(funcaoEmpresa);
+// => Exemplo 03 - Back Tick
+let nomeEmpresa = "Prisma";
+let dadosEmpresa = `Seja bem-vindo: ${nomeCompleto}! Você será: ${funcaoEmpresa}, na empresa ${nomeEmpresa}.`;
+console.log(dadosEmpresa);
---
+++ modulo_02/type_string/typeString.ts
@@ -0,0 +1,13 @@
+// => Exemplo 01 - Single Quote
+let nomeCompleto: string = "Matheus Vinicyus Strada";
+console.log(nomeCompleto);
+
+// => Exemplo 02 - Double Quote
+let funcaoEmpresa: string = "Analista de Suporte";
+console.log(funcaoEmpresa);
+
+// => Exemplo 03 - Back Tick
+let nomeEmpresa: string = "Prisma";
+
+let dadosEmpresa: string = `Seja bem-vindo: ${nomeCompleto}! Você será: ${funcaoEmpresa}, na empresa ${nomeEmpresa}.`;
+console.log(dadosEmpresa);
